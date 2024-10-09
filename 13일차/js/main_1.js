83cf824649ec8dc9fa7b03dc2705bf0efe53bf6a
elena7993 <elenalee7993@gmail.com>
2024-10-10 02:51:46 +0900

    10_10_24
    swiper_좋은거좋은거

---
+++ 13일차/js/main_1.js
@@ -0,0 +1,27 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+//   direction: 'vertical',
+// 기본값이 수평임
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+  effect: 'cards',
+  cardsEffect: {
+  }
+});
